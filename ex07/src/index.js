0c2c18513a6b4a92665b85644575b61414e2b923
BOZO <bozoperi@gmail.com>
2021-09-15 22:50:53 +0200

    few excerice added

---
+++ ex07/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+    //Only change code below this line
+    //Variable declaration
+var studlyCapVar;
+var properCamelCase;
+var titleCaseCover;
+    //Variable assigments
+studlyCapVar=10;
+properCamelCase="A String";
+titleCaseCover=9000;
+
+    //Only change code above this line
+return {studlyCapVar,
+    properCamelCase,
+    titleCaseCover}
+    
+}
+console.log(main());
+module.exports=main;
